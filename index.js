9b794ea7d144f057a8c71a497dfc647d3f0a33fc
Ville Leppänen <vileppanen@gmail.com>
2018-12-08 11:21:14 +0200

    fix: include new functions in bundle

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import List from './src/list'
 import ContainedNumber from './src/containedNumber'
 import Optional from './src/optional'
+import arrayAsProps from './src/arrayAsProps'
+import propsToArray from './src/propsToArray'
 
-export { ContainedNumber, List, Optional }
+export { ContainedNumber, List, Optional, arrayAsProps, propsToArray }
